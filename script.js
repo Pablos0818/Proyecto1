9499ec9f5a6e70994fa44df9197dd08eb22d07f0
alerodi <alerodi2001@yahoo.com>
2022-10-31 22:00:12 -0300

    footer con js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("idfooter").innerHTML = `
+    <a class="redsoc" href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://ar.pinterest.com/" target="_blank"><i class="fa fa-pinterest"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.instagram.com/?hl=es" target="_blank"><i class="fa fa-instagram"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.linkedin.com/" target="_blank"><i
+        class="fa fa-linkedin" aria-hidden="true"></i></a>
+    <p>Derechos reservados @2022 </p>
+`
